9a299736ae242518996fcfcc0ad83847661d0879
Tijesunimi Adebiyi <tijesu24@gmail.com>
2024-07-04 16:34:28 +0100

    still on this media issue o

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -158,9 +158,9 @@ EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
 # settings.py
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
-# AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
-# AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
 
 
 # AWS_DEFAULT_ACL = 'public-read'
@@ -280,3 +280,12 @@ THUMBNAIL_PROCESSORS = (
 )
 
 FILER_CANONICAL_URL = 'uploads/'
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {
+        },
+    },
+}
